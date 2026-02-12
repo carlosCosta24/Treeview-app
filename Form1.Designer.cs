f997f5758c727ec4c7bdb261a95d5a481156e53c
carlos_costa <carloscosta.pen@gmail.com>
2026-02-12 03:52:26 -0300

    Treeview with pic and selection fun

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,80 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("carlos");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("ricardo");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Boys", new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8});
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("karla", 1, 1);
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("aloha", 1, 1);
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Girls", 1, 1, new System.Windows.Forms.TreeNode[] {
+            treeNode10,
+            treeNode11});
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Boy.png");
+            this.imageList1.Images.SetKeyName(1, "Girl.png");
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(43, 45);
+            this.treeView1.Name = "treeView1";
+            treeNode7.Name = "Node2";
+            treeNode7.Text = "carlos";
+            treeNode8.Name = "Node3";
+            treeNode8.Text = "ricardo";
+            treeNode9.Name = "Node0";
+            treeNode9.SelectedImageIndex = 0;
+            treeNode9.Text = "Boys";
+            treeNode10.ImageIndex = 1;
+            treeNode10.Name = "Node4";
+            treeNode10.SelectedImageIndex = 1;
+            treeNode10.Text = "karla";
+            treeNode11.ImageIndex = 1;
+            treeNode11.Name = "Node5";
+            treeNode11.SelectedImageIndex = 1;
+            treeNode11.Text = "aloha";
+            treeNode12.ImageIndex = 1;
+            treeNode12.Name = "Node1";
+            treeNode12.SelectedImageIndex = 1;
+            treeNode12.Text = "Girls";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode9,
+            treeNode12});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(429, 458);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(528, 608);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace Treeview_app
         {
             InitializeComponent();
         }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(treeView1.SelectedNode.Text);
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            CheckAll(e.Node, e.Node.Checked);
+        }
+        private void CheckAll(TreeNode Node, Boolean IsChecked) {
+
+            foreach (TreeNode Child in Node.Nodes) { 
+                Child.Checked = IsChecked;
+                if (Child.Nodes.Count > 0) { 
+                    this.CheckAll(Child, IsChecked);
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TreeNode Family = new TreeNode("Family");
+
+            Family.Nodes.Add("koda");
+            Family.Nodes.Add("lua");
+            treeView1.Nodes.Add(Family);
+
+        }
     }
 }
